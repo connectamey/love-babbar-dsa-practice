824639c23da4a6b89006cdf1c9bde4bc6c2452ce
connectamey <17310788-connectamey@users.noreply.replit.com>
2023-11-26 23:19:24 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@lec-3-hw/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+  
+  while(int i < 100)
+  return 0;
+}
+
+//Find sum of all even numbers less than 100
---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+  int i = 0;
+  int sum = 0;
+
+  while(i <= 100)
+    {
+     sum = sum + i;
+     i = i + 1;
+    }
+  cout << "sum of all even numbers is "<<sum;
+    return 0;
+}
+
+//Find sum of all even numbers less than 100
